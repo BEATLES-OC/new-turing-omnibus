bc4264dca41b18e96b980c4a692e5722330dc884
Jeremy Ashkenas <jashkenas@gmail.com>
2008-09-11 22:36:24 -0400

    sorta working first algorithm

--- 01_algorithms/wallpaper_1.rb
+++ 01_algorithms/wallpaper_1.rb
@@ -1,24 +1,44 @@
-class Wallpaper < Processing::App
+# Page 3's algorithm.
+# Was broken for a long time because I always forget 
+# that Ruby rounds your int math. Fuck.
+# Still seems wrong. Something smells fishy.
+
+require 'ruby-processing'
+
+class Wallpaper1 < Processing::App
   has_slider :corna
   has_slider :cornb
   has_slider :side
-  
+
   def setup
-    background black
-    fill white
+    color_mode RGB, 1.0
+    background 0.0
+    fill 1.0
+    stroke 1.0
+    
+    @corna = 10.0
+    @cornb = 10.0
+    @side = 20.0
+    
+    @i = 0
+  end
+  
+  def draw
+    return if @i > 300
+    @i += 1
+    300.times do |j|
+      x = (@corna + @i.to_f) * @side / 300.0
+      y = (@corna + j.to_f) * @side / 300.0
+      c = ((x*x) + (y*y)).to_i
+      point(j, @i) if c % 2 == 0
+    end
   end
   
   def mouse_pressed
-    100.times do |i|
-      100.times do |j|
-        x = @corna + i * @side / 100
-        y = @cornb + j * @side / 100
-        c = (x**2 + y**2).to_i
-        point(i, j) if (c % 2 == 0)
-      end
-    end
+    background 0.0
+    @i = 0
   end
   
 end
 
-Wallpaper.new :title => "Wallpaper 1"
+Wallpaper1.new :title => "Wallpaper 1", :width => 300, :height => 300
